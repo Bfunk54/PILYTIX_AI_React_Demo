cc6d20f81d92f4bb435b9ad696ed3959f62246a9
Bfunk54 <ben_fein@icloud.com>
2023-02-09 15:22:42 -0500

    a lot of work on the header, pretty much done for now, added in all buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./styles.css";
 import BasicTable from "./Components/Table";
 import NavBar from "./Components/Header"
+import Typography from "@mui/material/Typography"
 
 export default function App() {
   return (
     <div className="App">
       <NavBar/>
-      <h2>PILYTIX Scored Opportunities</h2>
+      <Typography className="scored" variant="h4" component="h2">PILYTIX Scored Opportunities</Typography>
       <BasicTable></BasicTable>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,27 @@
 import * as React from "react";
-import { Box, Toolbar, Typography, Container, AppBar, Link } from "@mui/material";
-import logo from "../images/pilytix-logo.png"
+import {
+  Box,
+  Toolbar,
+  Typography,
+  Container,
+  AppBar,
+  Link,
+  Button,
+} from "@mui/material";
+import logo from "../images/pilytix-logo.png";
+import "./Header/Header.css";
 
 export default function NavBar() {
   return (
     <AppBar
       sx={{ borderBottomLeftRadius: "40px", borderBottomRightRadius: "40px" }}
       position="fixed"
+      className="nav"
     >
-        <Container maxWidth="xl">
-        <Toolbar className="logo" disableGutters>
+      <Container>
+        <Toolbar disableGutters>
           <Box
-            style={{
+            sx={{
               display: "flex",
               flexWrap: "nowrap",
               alignItems: "center",
@@ -20,19 +30,127 @@ export default function NavBar() {
             <Box
               sx={{
                 alignItems: "center",
-                display: "flex"
+                display: "flex",
               }}
             >
               <Box sx={{ display: "flex" }}>
                 <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-                  <img height="80px" width="200px" src={logo} />
+                  <img height="80px" width="210px" src={logo} />
                 </Link>
               </Box>
-              </Box>
-              </Box>
-              </Toolbar>
-              </Container>
+            </Box>
+            <Box
+              sx={{
+                justifyContent: "flex-end",
+                alignItems: "center",
+                flexGrow: 1,
+                marginLeft: "20px",
+                display: { xs: "none", sm: "none", md: "flex" },
+              }}
+            >
+              <Link to="/solutions" sx={{ textDecoration: "none" }}>
+                <Button
+                  size="small"
+                  variant="contained"
+                  sx={{
+                    my: 2,
+                    backgroundColor: "#3abaff",
+                    color: "white",
+                    display: "block",
+                    marginRight: "20px",
+                    textTransform: "none",
+                    fontSize: "16px",
+                  }}
+                >
+                  Solutions
+                </Button>
+              </Link>
+
+              <Link to="/insights" sx={{ textDecoration: "none" }}>
+                <Button
+                  size="small"
+                  variant="contained"
+                  sx={{
+                    my: 2,
+                    backgroundColor: "#3abaff",
+                    color: "white",
+                    display: "block",
+                    marginRight: "20px",
+                    textTransform: "none",
+                    fontSize: "16px",
+                  }}
+                >
+                  Insights
+                </Button>
+              </Link>
+
+              <Link to="/about-us" sx={{ textDecoration: "none" }}>
+                <Button
+                  size="small"
+                  variant="contained"
+                  sx={{
+                    my: 2,
+                    backgroundColor: "#3abaff",
+                    color: "white",
+                    display: "block",
+                    marginRight: "20px",
+                    textTransform: "none",
+                    fontSize: "16px",
+                  }}
+                >
+                  About Us
+                </Button>
+              </Link>
+
+              <Link to="/contact-us" sx={{ textDecoration: "none" }}>
+                <Button
+                  size="small"
+                  variant="contained"
+                  sx={{
+                    my: 2,
+                    backgroundColor: "#3abaff",
+                    color: "white",
+                    display: "block",
+                    marginRight: "20px",
+                    textTransform: "none",
+                    fontSize: "16px",
+                  }}
+                >
+                  Contact Us
+                </Button>
+              </Link>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              justifyContent: "flex-end",
+              alignItems: "center",
+              flexGrow: 1,
+              marginLeft: "20px",
+              display: { xs: "none", sm: "none", md: "flex" },
+            }}
+          >
+            <Link to="/request-demo" sx={{ textDecoration: "none" }}>
+              <Button
+                size="small"
+                variant="contained"
+                sx={{
+                  my: 2,
+                  backgroundColor: "#34e4c4",
+                  color: "white",
+                  display: "block",
+                  marginLeft: "-1px",
+                  borderRadius: "30px",
+                  textTransform: "none",
+                  fontSize: "20px",
+                }}
+              >
+                Request Demo
+              </Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
-    
   );
 }
