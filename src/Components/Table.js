73c333b674622061e125eb2615c75a718348ab12
Bfunk54 <ben_fein@icloud.com>
2023-02-10 10:03:25 -0500

    got data to sort by amount

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,6 +8,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import TableSortLabel from "@mui/material/TableSortLabel";
 import "./Table/Table.css"
 
 import * as opportunities from "./Table/opportunities.json";
@@ -20,6 +22,28 @@ export default function BasicTable() {
   function handleRowClick(event, row) {
     console.log("row", row);
   }
+  
+  const [rowData, setRowData] = useState(data);
+  const [orderDirection, setOrderDirection] = useState("asc");
+
+  const sortArray = (arr, orderBy) => {
+    switch (orderBy) {
+      case "asc":
+      default:
+        return arr.sort((a, b) =>
+          a.amount > b.amount ? 1 : b.amount > a.amount ? -1 : 0
+        ); 
+      case "desc":
+        return arr.sort((a, b) =>
+          a.amount < b.amount ? 1 : b.amount < a.amount ? -1 : 0
+        );
+    }
+  };
+   
+  const handleSortRequest = () => {
+    setRowData(sortArray(data, orderDirection));
+    setOrderDirection(orderDirection === "asc" ? "desc" : "asc");
+  };
 
   return (
 
@@ -37,13 +61,17 @@ export default function BasicTable() {
             <TableCell align="right">Rep Probability</TableCell>
             <TableCell align="right">PX Probability</TableCell>
             <TableCell align="left">PX Tier</TableCell>
-            <TableCell align="right">Amount</TableCell>
+            <TableCell align="right" onClick={handleSortRequest}>
+            <TableSortLabel active={true} direction={orderDirection}>
+              Amount
+              </TableSortLabel>
+              </TableCell>
             <TableCell align="left">Product</TableCell>
             <TableCell align="left">Sales Rep</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row) => (
+          {rowData.map((row) => (
             <TableRow
               onClick={(event) => handleRowClick(event, row)}
               key={row.oppId}
@@ -56,7 +84,9 @@ export default function BasicTable() {
               <TableCell align="right">{row.repProbability}</TableCell>
               <TableCell align="right">{row.pilytixProbability}</TableCell>
               <TableCell align="left">{row.pilytixTier}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+              <TableCell align="right">
+                {row.amount}
+                </TableCell>
               <TableCell align="left">{row.product}</TableCell>
               <TableCell align="left">{row.salesRepName}</TableCell>
             </TableRow>
